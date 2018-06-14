26f582cff896f826b9225afef6b09d9a3a4b51f8
PacoRodriguez-InfoTrack <Paco.Rodriguez@infotrack.co.uk>
2018-06-14 01:26:28 +0100

    Prototype 1

---
+++ Tailor/Controllers/Prototype1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tailor.Controllers
+{
+    public class Prototype1Controller : Controller
+    {
+        // GET: Prototype1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tailor/Models/PanelModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Tailor.Models
+{
+    public class PanelModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public PanelBody Body { get; set; } = new PanelBody();
+        public string Info { get; set; }
+        public string Price { get; set; }
+
+        public bool HasInfo()
+        {
+            return !string.IsNullOrEmpty(Info);
+        }
+    }
+
+    public class PanelBody
+    {
+        public bool IncludeType { get; set; }
+        public bool IncludeStatus { get; set; }
+        public PanelDateFilter DateFilter { get; set; } = new PanelDateFilter();
+    }
+
+    public class PanelDateFilter
+    {
+        public bool Include { get; set; }
+        public string Title { get; set; }
+    }
+}
